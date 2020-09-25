d63aa0dd16c1e692de494da953a4acd1a0c7bcfd
daniel21h <hesseldaniel21@gmail.com>
2020-09-25 16:00:04 -0300

    feat: typeorm repository

--- src/database/migrations/1601055158915-CreateAppointments.ts
+++ src/database/migrations/1601055158915-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1601055158915
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
