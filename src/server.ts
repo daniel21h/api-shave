199000a552cdb0f5f006609cba6bd286da911808
daniel21h <hesseldaniel21@gmail.com>
2020-09-25 14:28:33 -0300

    feat: typeorm setup

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 const server = express();
 
 server.use(express.json());
 server.use(routes);
-
-server.get('/', (request, response) =>
-  response.json({ message: 'Hello GoStack' }),
-);
 
 server.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
