5202cd1c772ea8851fd72bce6e14d6cc603b70c9
daniel21h <hesseldaniel21@gmail.com>
2020-09-25 11:37:01 -0300

    feat: registration of appointments

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
-import express from "express";
+import express from 'express';
+import routes from './routes';
 
 const server = express();
 
-server.get("/", (request, response) => {
-  return response.json({ message: "Hello GoStack" });
-});
+server.use(express.json());
+server.use(routes);
+
+server.get('/', (request, response) =>
+  response.json({ message: 'Hello GoStack' }),
+);
 
 server.listen(3333, () => {
-  console.log("🚀 Server started on port 3333!");
+  console.log('🚀 Server started on port 3333!');
 });
